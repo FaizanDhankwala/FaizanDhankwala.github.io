a5aaff87d9c37a889b7d4642ed132549bcc46f89
Faizan Dhankwala on School Machine <faizan.dhankwala@bellevuecollege.edu>
2019-11-13 12:17:11 -0800

    function-basics

---
+++ javascript-tests/function-basics.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    // All code goes in here.
+    
+const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+const functionCallsAction = document.getElementById('functionCallsAction');
+
+functionCallsAction.onclick = simpleFunction;
+
+
+
+ function simpleFunction(){
+    console.log("functions should consist of statements designed to perform a single task.")
+    simpleFunctionDisplay.textContent = "simple";
+    
+}   
+//simpleFunction();
+}
+function functionParameters(value){
+    console.log(value);
+}
+function functionReturn(){
+    return "Many functions return values.";
+}
+
+function add(a,b){
+    return a + b;
+}
+
+functionParameters( "Many functions take parameters.");
+const returnValue = functionReturn();
+console.log(returnValue);
+
+const result= add(2,3);
+console.log(result);
