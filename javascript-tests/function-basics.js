37bd29e07a3ce2a21fffd00513379bf24776ee1a
FaizanDhankwala <55712375+FaizanDhankwala@users.noreply.github.com>
2019-11-15 08:52:06 -0800

    functions

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -32,3 +32,19 @@ console.log(returnValue);
 
 const result= add(2,3);
 console.log(result);
+
+const data = nameDisplay.value;         
+
+const elfCode = {
+    appendToList: (list, value) => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(value));
+        list.appendChild(li);
+    }
+}
+
+const someAction = document.getElementById('someAction');
+someAction.onclick = function() {
+    // Handle the onclick event in here.
+}
+
