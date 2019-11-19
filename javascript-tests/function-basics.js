50b44b27bfb60767abe2ad19c391661fde1dba4f
FaizanDhankwala <55712375+FaizanDhankwala@users.noreply.github.com>
2019-11-18 18:01:55 -0800

    function basics

--- javascript-tests/function-basics.js
+++ javascript-tests/function-basics.js
@@ -1,60 +1,64 @@
 window.onload = () => {
     // All code goes in here.
-    
-const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
-const functionCallsAction = document.getElementById('functionCallsAction');
-
-functionCallsAction.onclick = simpleFunction;
-
-
-
- function simpleFunction(){
-    console.log("functions should consist of statements designed to perform a single task.")
-    simpleFunctionDisplay.textContent = "simple";
-
-    function simple() {
-        console.log('simple');
-        simpleFunctionDisplay.textContent = "simple";
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
     }
     
-}   
-//simpleFunction();
+
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+    const addToListAction = document.getElementById('addToListAction');
+    functionCallsAction.onclick = simpleFunction;
+
+
+
+    function simpleFunction() {
+        console.log("functions should consist of statements designed to perform a single task.")
+        simpleFunctionDisplay.textContent = "simple";
+
+        function simple() {
+            console.log('simple');
+            simpleFunctionDisplay.textContent = "simple";
+        }
+       
+    }
+    addToListAction.onclick = function () {
+        // Handle the onclick event in here.
+         const listDisplay= document.getElementById('listDisplay');
+        const nameDisplay=document.getElementById('nameDisplay');
+        const userInput=nameDisplay.value;
+        console.log('userInput',userInput);
+        elfCode.appendToList(listDisplay,userInput);
+    }
+    //simpleFunction();
 }
-function functionParameters(value){
+function functionParameters(value) {
     console.log(value);
 }
-function functionReturn(){
+function functionReturn() {
     return "Many functions return values.";
 }
 
-function add(a,b){
+function add(a, b) {
     return a + b;
 }
 
-functionParameters( "Many functions take parameters.");
+functionParameters("Many functions take parameters.");
 const returnValue = functionReturn();
 console.log(returnValue);
 
-const result= add(2,3);
+const result = add(2, 3);
 console.log(result);
 
-const data = nameDisplay.value;         
+//const data = nameDisplay.value;         
 
-const elfCode = {
-    appendToList: (list, value) => {
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(value));
-        list.appendChild(li);
-    }
-}
 
-const someAction = document.getElementById('someAction');
-someAction.onclick = function() {
-    // Handle the onclick event in here.
-}
 
-document.getElementById('addToListAction');
 
-document.getElementById('listDisplay');
-document.getElementById('nameDisplay').value
+
+
 
