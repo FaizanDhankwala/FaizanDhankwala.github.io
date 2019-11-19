9f062bbc2cf8b1ff3e051881da31604decc86f7f
FaizanDhankwala <55712375+FaizanDhankwala@users.noreply.github.com>
2019-11-19 11:01:38 -0800

    java

--- javascript-tests/javascripts-objects-work.js
+++ javascript-tests/javascripts-objects-work.js
@@ -7,14 +7,32 @@ return this.firstName +' '+ this.lastName;
 };
 
 var calculator={
-    operand: 1,
-    operand:2,
-    addMe: function(){
-        return this.operand +this.operand;
-    }
+ operand01:'-1',
+ operand02:'-1',
+  x = 5,
+  y= 5,        
+ z = x + y,
+ z= y - x,
+};
+calculator.operand01 = person.firstName.length;
 
+
+
+
+console.log(calculator.operand01);
+console.log(calculator.operand02);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName());
+
+
+
+divider('Calculator');
+console.log('operand01 =', calculator.operand01);
+
+
+function divider(title) {
+    console.log("\n====================================");
+    console.log(title);
+    console.log("====================================\n");
 }
-console.log(myObject01.firstName);
-console.log(myObject01.lastName);
-console.log(myObject02.fullName());
-console.log(myObject03.addMe());
