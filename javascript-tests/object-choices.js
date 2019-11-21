0c43a51711684daeacd8415dd17ac329250705ad
Faizan Dhankwala <faizandhankwala1@gmail.com>
2019-11-20 18:14:42 -0800

    JaMes WAAAAAAAAAAAAAAATSON

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,19 +1,16 @@
 window.onload = () => {
 
     const simpleObjectDisplay = document.getElementById('simpleObjectDisplay');
-    const simpleObjectAction = document.getElementById('simpleObjectAction');
-    // simpleObjectDisplay.textContent= 'Bruh moment'; 
-
-
+    const simpleObjectAction = document.getElementById('simpleOjectAction');
+    
     const simpleObject = {
         firstName: 'James',
-        lastName: "Watson",
+        lastName: 'Watson',
         sayName: function () {
-
-            const fullName =this.firstName + " " + this.lastName);
-            console.log(fullName);
+            const fullName = this.firstName + " " + this.lastName;
+           // console.log(fullName);
             simpleObjectDisplay.textContent = fullName;
-        }
+            }
     };
 
     simpleObjectAction.onclick = function(){
@@ -22,29 +19,28 @@ window.onload = () => {
 
     simpleObject.dynamicMethod = () => {
         console.log('Dynamic Method');
-
-    }
+    };
 
     function FunctionObject() {
         FunctionObject.prototype.sayName = function () {
-            console.log('Function Object')
-
-
+            console.log('Function Object');
         }
     };
 
     class CustomClass {
         sayName() {
-            console.log('CustomClass');
+            console.log('Custom Class');
         }
-    }
-    simpleObject.sayName();
-    simpleObject.dynamicMethod();
+    };
 
     const functionObject = new FunctionObject();
-    functionObject.sayName();
-
     const customClass = new CustomClass();
+    //simpleObject.sayName();
+    simpleObject.dynamicMethod();
+    functionObject.sayName();
     customClass.sayName();
 
-}
+
+
+
+};
